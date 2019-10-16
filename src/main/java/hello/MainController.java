d39d026f0786c56bdcdb907e408dd7dcb663ae52
Jeremy Chandler <goldenmeso@gmail.com>
2019-10-16 00:47:52 -0600

    project structure

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -8,6 +8,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MainController {
 
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
